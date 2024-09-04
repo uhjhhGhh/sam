c668d5a78ef632f4924120489c0149f1feb9141d
uhjhhGhh <balidabhishek8@gmail.com>
2024-09-04 20:43:42 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY L4D SAM
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7235368503:AAF5ld7gbMj6mG_WhqUBR0R45Nw-DnwPE2E")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("samy784")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("6207079474")
+
+
+
+
+
+
+
+  
